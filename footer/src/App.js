cb277f74ab24bb2e58326f55fd19666de68a59ee
akshitaanand <63819293+akshitaanand@users.noreply.github.com>
2020-07-26 19:55:49 -0500

    Add files via upload

---
+++ footer/src/App.js
@@ -0,0 +1,17 @@
+import React from "react";
+import "./App.css";
+import Header from "./Header";
+import Footer from "./Footer";
+
+export default function App() {
+  return (
+    <div className="main-container">
+      <header>
+        <Header />
+      </header>
+<footer>
+      <Footer />
+</footer>
+    </div>
+  );
+}
